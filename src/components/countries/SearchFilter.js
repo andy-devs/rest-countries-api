a965f74857a96309e3f31452728b711524d20e64
andy-devs <markushin.andrew@gmail.com>
2022-03-28 16:32:44 +0600

    styled search and filter

--- src/components/countries/SearchFilter.js
+++ src/components/countries/SearchFilter.js
@@ -1,15 +1,26 @@
 import styles from './SearchFilter.module.css';
 import { TextInput, Select } from '@mantine/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchFilter = () => {
 	return (
 		<div className={styles['search-filter']}>
 			<TextInput
+				className={styles.input}
+				variant='unstyled'
 				type='text'
 				name='search'
 				placeholder='Search for a country...'
+				icon={<FontAwesomeIcon icon={faMagnifyingGlass} size='sm' />}
 			/>
 			<Select
+				classNames={{
+					dropdown: styles.select__drop,
+					unstyledVariant: styles.select,
+				}}
+				clearable
+				variant='unstyled'
 				placeholder='Filter by region'
 				data={[
 					{ value: 'africa', label: 'Africa' },
