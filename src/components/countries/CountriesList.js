e1d3a98877207fe60d09b22360c129511c26cd7b
andy-devs <markushin.andrew@gmail.com>
2022-03-28 18:59:26 +0600

    loader, error on load

--- src/components/countries/CountriesList.js
+++ src/components/countries/CountriesList.js
@@ -1,24 +1,72 @@
 import styles from './CountriesList.module.css';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Oval } from 'react-loader-spinner';
+
+import SearchFilter from '../countries/SearchFilter';
 import Country from './Country';
 
 const CountriesList = () => {
 	const [countries, setCountries] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
+
+	const location = useLocation();
+
+	const searchParams = new URLSearchParams(location.search);
 
 	useEffect(() => {
 		const fetchAllCountries = async () => {
-			const response = await fetch('https://restcountries.com/v2/all');
-			const data = await response.json();
-			console.log(data);
-			setCountries(data);
+			try {
+				setIsLoading(true);
+				const response = await fetch('https://restcountries.com/v2/all');
+				const data = await response.json();
+				setCountries(data);
+				setIsLoading(false);
+			} catch (err) {
+				setIsLoading(false);
+				setError(err.message);
+			}
 		};
 		fetchAllCountries();
 	}, []);
 
-	return (
-		<div className={styles['countries-grid']}>
-			{countries.length > 0 &&
-				countries.map((country) => (
+	let content = (
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				marginTop: '2rem',
+			}}>
+			<Oval
+				ariaLabel='loading-indicator'
+				height={100}
+				width={100}
+				strokeWidth={3}
+				color='black'
+				secondaryColor='white'
+			/>
+		</div>
+	);
+
+	if (error) {
+		content = (
+			<p
+				style={{
+					'text-align': 'center',
+					'font-size': '1.5rem',
+					'margin-top': '2rem',
+				}}>
+				{error}
+			</p>
+		);
+	}
+
+	if (!isLoading && !error) {
+		content = (
+			<div className={styles['countries-grid']}>
+				{countries.map((country) => (
 					<Country
 						key={country.numericCode}
 						name={country.name}
@@ -28,7 +76,15 @@ const CountriesList = () => {
 						capital={country.capital}
 					/>
 				))}
-		</div>
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<SearchFilter />
+			{content}
+		</>
 	);
 };
 
--- src/pages/AllCountries.js
+++ src/pages/AllCountries.js
@@ -1,15 +1,8 @@
-import { useLocation } from 'react-router-dom';
-import SearchFilter from '../components/countries/SearchFilter';
 import CountriesList from '../components/countries/CountriesList';
 
 const AllCountries = () => {
-	const location = useLocation();
-
-	const searchParams = new URLSearchParams(location.search);
-
 	return (
 		<main className='main'>
-			<SearchFilter />
 			<CountriesList />
 		</main>
 	);
